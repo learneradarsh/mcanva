737b63bb4f6a346ab02dd004166458b297fc0b93
Adarsh Learner <learner.adarsh@gmail.com>
2022-08-09 23:09:42 +0530

    listed all templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mycanva';
+  cardTemplates = [
+    {
+      id: 1,
+      templateUrl: '../assets/temp1.png',
+    },
+    {
+      id: 2,
+      templateUrl: '../assets/temp2.png',
+    },
+    {
+      id: 3,
+      templateUrl: '../assets/temp3.png',
+    },
+    {
+      id: 4,
+      templateUrl: '../assets/temp4.png',
+    },
+    {
+      id: 5,
+      templateUrl: '../assets/temp5.png',
+    }
+  ]
 }
--- src/app/template-card/template-card.component.ts
+++ src/app/template-card/template-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-template-card',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateCardComponent implements OnInit {
 
+  @Input() cardInfo: any;
   constructor() { }
 
   ngOnInit(): void {
